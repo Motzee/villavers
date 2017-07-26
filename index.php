78bdef7a8506d074404268ccafbe4082cba1da41
Maelle <maelle.perron.26@gmail.com>
2017-07-26 15:05:00 +0200

    Synthèse d'une grille de base via php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$garden = file_get_contents('garden.json') ;
+
+$garden = json_decode($garden, true);
+
+echo '<table id="garden">' ;
+echo '<thead><tr><th></th>' ;
+    for($nbcol = 1; $nbcol <= 10; $nbcol++) {
+        echo "<th>$nbcol</th>" ;
+    }
+echo '</tr></thead><tbody>' ;
+
+foreach($garden as $ligne => $cases) {
+    echo "<tr><td>$ligne</td>" ;
+    foreach($cases as $coord => $case) {
+        echo '<td id='.$coord.'></td>' ;
+    }
+    echo "</tr>" ;
+}
+
+echo '</tbody></table>' ;
+
+?>
+
+<link rel="stylesheet" href="styles.css" />
